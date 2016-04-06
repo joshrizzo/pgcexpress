7e9b245d97b8836a40a0166252d4b7c19d438295
jrizzo <josh.rizzo@gmail.com>
2016-04-06 12:11:07 -0500

    moved the test dependency to dev dependencies

---
+++ spec/dataLayerSpec.js
@@ -0,0 +1,10 @@
+﻿var dataLayer = require('../services/dataLayer.js');
+
+describe("dataLayer", function () {
+	describe("getAll", function () {
+		it("returns nothing first", function () {
+			var results = dataLayer.getAll();
+			expect(results.length).toBe(0);
+		});
+	});
+});
